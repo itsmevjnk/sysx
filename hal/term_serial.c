6a728054cb0dfc76755852d022da2abd9b2cd440
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2023-11-02 17:53:55 +0700

    Updated serial terminal implementation

--- hal/term_serial.c
+++ hal/term_serial.c
@@ -28,14 +28,7 @@
 #define TERM_SER_BAUD               115200UL
 #endif
 
-void term_init() {
-    ser_init(TERM_SER_PORT, TERM_SER_DBIT, TERM_SER_SBIT, TERM_SER_PARITY, TERM_SER_BAUD);
-#ifndef TERM_NO_CLEAR // to save space and time
-    term_clear();
-#endif
-}
-
-void term_putc(char c) {
+void serterm_putc(const term_hook_t* impl, char c) {
 #ifndef TERM_SER_NO_CRNL
     if(c == '\r') return; // ignore CR (since we'll be converting NL to CR+NL anyway)
     if(c == '\n') ser_putc(TERM_SER_PORT, '\r'); // add CR to NL
@@ -44,19 +37,23 @@ void term_putc(char c) {
 }
 
 #ifndef TERM_NO_INPUT
-char term_getc_noecho() {
+size_t serterm_available(const term_hook_t* impl) {
+    return (ser_avail_write(TERM_SER_PORT)) ? 1 : 0;
+}
+
+char serterm_getc(const term_hook_t* impl) {
     return ser_getc(TERM_SER_PORT);
 }
 #endif
 
 #ifndef TERM_NO_CLEAR
-void term_clear() {
+void serterm_clear(const term_hook_t* impl) {
     ser_puts(TERM_SER_PORT, "\x1B[2J\x1B[H"); // clear entire screen, then move back to home position (ANSI)
 }
 #endif
 
 #ifndef TERM_NO_XY
-void term_get_dimensions(size_t* width, size_t* height) {
+void serterm_get_dimensions(const term_hook_t* impl, size_t* width, size_t* height) {
     size_t x, y; // for saving the current cursor position
     term_get_xy(&x, &y);
     term_set_xy(1000, 1000); // move the cursor to some outrageous position
@@ -65,11 +62,11 @@ void term_get_dimensions(size_t* width, size_t* height) {
     term_set_xy(x, y); // restore cursor position
 }
 
-void term_set_xy(size_t x, size_t y) {
+void serterm_set_xy(const term_hook_t* impl, size_t x, size_t y) {
     kprintf("\x1B[%u;%uH", y, x); // since kstdout is set to the terminal, we can use kprintf normally
 }
 
-void term_get_xy(size_t* x, size_t* y) {
+void serterm_get_xy(const term_hook_t* impl, size_t* x, size_t* y) {
     ser_puts(TERM_SER_PORT, "\x1B[6n"); // Device Status Report: returns cursor position
     
     *x = 0; *y = 0; // so we can start writing the results straight into them
@@ -99,5 +96,44 @@ void term_get_xy(size_t* x, size_t* y) {
     }
 }
 #endif
+
+const term_hook_t serterm_hook = {
+    &serterm_putc,
+    NULL,
+#ifndef TERM_NO_INPUT
+    &serterm_available,
+    &serterm_getc,
+#else
+    NULL,
+    NULL,
+#endif
+
+#ifndef TERM_NO_CLEAR
+    &serterm_clear,
+#else
+    NULL,
+#endif
+
+#ifndef TERM_NO_XY
+    &serterm_get_dimensions,
+    &serterm_set_xy,
+    &serterm_get_xy,
+#else
+    NULL,
+    NULL,
+    NULL,
+#endif
+
+    NULL
+};
+
+void term_init() {
+    ser_init(TERM_SER_PORT, TERM_SER_DBIT, TERM_SER_SBIT, TERM_SER_PARITY, TERM_SER_BAUD);
+#ifndef TERM_NO_CLEAR // to save space and time
+    serterm_clear(NULL);
+#endif
+
+    term_impl = &serterm_hook;
+}
 
 #endif
