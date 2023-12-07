579b651ea78542a58be07b5a7f3afbd63ef9abc5
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2023-12-07 16:15:27 +0700

    Added kernel cmdline option for disabling ACPI

--- drivers/acpi.c
+++ drivers/acpi.c
@@ -1,9 +1,18 @@
 #include <drivers/acpi.h>
 #include <kernel/log.h>
+#include <kernel/cmdline.h>
+#include <string.h>
 
 #ifdef FEAT_ACPI
 
 bool acpi_init() {
+    /* check if ACPI is disabled according to cmdline */
+    const char* acpi_state = cmdline_find_kvp("acpi");
+    if(acpi_state != NULL && !strcmp(acpi_state, "off")) {
+        kinfo("ACPI support is disabled via cmdline");
+        return false;
+    }
+    
     if(!acpi_arch_init()) {
         kerror("acpi_arch_init() failed");
         return false;
