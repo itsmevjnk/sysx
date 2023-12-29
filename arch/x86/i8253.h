bda84831399d877995d02f434528944b84ee4a54
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2023-12-29 12:16:46 +0700

    Improved PIT system timer accuracy to microsecond-level

--- arch/x86/i8253.h
+++ arch/x86/i8253.h
@@ -4,6 +4,20 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/* PIT clock frequency */
+#ifndef PIT_FREQ
+#define PIT_FREQ                1193182 // 1.193182 MHz
+#endif
+
+/* desired system timer frequency */
+#ifndef PIT_SYSTIMER_FREQ
+#define PIT_SYSTIMER_FREQ       1000
+#endif
+
+#define PIT_DIVISOR             ((uint16_t)(PIT_FREQ / PIT_SYSTIMER_FREQ)) // the divisor value to be fed into the PIT
+
+#define PIT_SYSTIMER_FREQ_REAL  ((float)PIT_FREQ / (float)PIT_DIVISOR) // the resulting (actual) system timer frequency
+
 /*
  * void pit_systimer_init()
  *  Initializes the PIT's channel 0 to work as the system timer
