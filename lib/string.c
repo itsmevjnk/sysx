0f6b08225ca15c7513eaf1c20f2103dd667469f2
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2024-01-10 11:38:01 +0700

    Allow stock (unoptimized) string functions to be overridden without specifying ARCH_ build macros by declaring them as weak symbols

--- lib/string.c
+++ lib/string.c
@@ -1,17 +1,14 @@
 #include <string.h>
 
-#ifndef ARCH_MEMCPY
-void* memcpy(void* dest, const void* src, size_t n) {
+__attribute__((weak)) void* memcpy(void* dest, const void* src, size_t n) {
     if((uintptr_t) dest == (uintptr_t) src) return dest;
     for(size_t i = 0; i < n; i++) {
         *(uint8_t*)((uintptr_t) dest + i) = *(uint8_t*)((uintptr_t) src + i);
     }
     return dest;
 }
-#endif
 
-#ifndef ARCH_MEMMOVE
-void* memmove(void* dest, const void* src, size_t n) {
+__attribute__((weak)) void* memmove(void* dest, const void* src, size_t n) {
     if((uintptr_t) dest == (uintptr_t) src) return dest;
     if((uintptr_t) dest < (uintptr_t) src) {
         for(size_t i = 0; i < n; i++) {
@@ -24,46 +21,36 @@ void* memmove(void* dest, const void* src, size_t n) {
     }
     return dest;
 }
-#endif
 
-#ifndef ARCH_MEMSET
-void* memset(void* str, int c, size_t n) {
+__attribute__((weak)) void* memset(void* str, int c, size_t n) {
     for(size_t i = 0; i < n; i++) {
         *(uint8_t*)((uintptr_t) str + i) = (uint8_t) c;
     }
     return str;
 }
-#endif
 
-#ifndef ARCH_MEMSET16
-void* memset16(void* str, uint16_t c, size_t n) {
+__attribute__((weak)) void* memset16(void* str, uint16_t c, size_t n) {
     for(size_t i = 0; i < n; i++) {
         *(uint16_t*)((uintptr_t) str + i) = c;
     }
     return str;
 }
-#endif
 
-#ifndef ARCH_MEMSET32
-void* memset32(void* str, uint32_t c, size_t n) {
+__attribute__((weak)) void* memset32(void* str, uint32_t c, size_t n) {
     for(size_t i = 0; i < n; i++) {
         *(uint32_t*)((uintptr_t) str + i) = c;
     }
     return str;
 }
-#endif
 
-#ifndef ARCH_MEMSET64
-void* memset64(void* str, uint64_t c, size_t n) {
+__attribute__((weak)) void* memset64(void* str, uint64_t c, size_t n) {
     for(size_t i = 0; i < n; i++) {
         *(uint64_t*)((uintptr_t) str + i) = c;
     }
     return str;
 }
-#endif
 
-#ifndef ARCH_MEMCMP
-int memcmp(const void* ptr1, const void* ptr2, size_t n) {
+__attribute__((weak)) int memcmp(const void* ptr1, const void* ptr2, size_t n) {
     const uint8_t* buf1 = ptr1;
     const uint8_t* buf2 = ptr2;
     int ret = 0;
@@ -72,10 +59,8 @@ int memcmp(const void* ptr1, const void* ptr2, size_t n) {
     }
     return ret;
 }
-#endif
 
-#ifndef ARCH_STRCMP
-int strcmp(const char* str1, const char* str2) {
+__attribute__((weak)) int strcmp(const char* str1, const char* str2) {
     if((uintptr_t) str1 == (uintptr_t) str2) return 0;
     int ret = 0;
     for(size_t i = 0; ret == 0; i++) {
@@ -84,10 +69,8 @@ int strcmp(const char* str1, const char* str2) {
     }
     return ret;
 }
-#endif
 
-#ifndef ARCH_STRNCMP
-int strncmp(const char* str1, const char* str2, size_t n) {
+__attribute__((weak)) int strncmp(const char* str1, const char* str2, size_t n) {
     if((uintptr_t) str1 == (uintptr_t) str2) return 0;
     int ret = 0;
     for(size_t i = 0; i < n && ret == 0; i++) {
@@ -96,25 +79,18 @@ int strncmp(const char* str1, const char* str2, size_t n) {
     }
     return ret;
 }
-#endif
 
-#ifndef ARCH_STRLEN
-size_t strlen(const char* str) {
+__attribute__((weak)) size_t strlen(const char* str) {
     int ret = -1;
     while(str[++ret]);
     return (size_t) ret;
 }
-#endif
 
-#ifndef ARCH_STRCPY
-char* strcpy(char* dest, const char* src) {
+__attribute__((weak)) char* strcpy(char* dest, const char* src) {
     return memmove(dest, src, strlen(src) + 1);
 }
-#endif
 
-#ifndef ARCH_STRNCPY
-char* strncpy(char* dest, const char* src, size_t n) {
+__attribute__((weak)) char* strncpy(char* dest, const char* src, size_t n) {
     size_t srclen = strlen(src);
     return memmove(dest, src, (srclen < n) ? (srclen + 1) : n);
 }
-#endif
