272ae7c6cdff8ebda947bf3ea9ef436c3dda96af
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2024-01-12 00:40:41 +0700

    Made mutex functions overrideable for architecture-specific truly-atomic implementations

--- helpers/mutex.c
+++ helpers/mutex.c
@@ -1,20 +1,16 @@
 #include <helpers/mutex.h>
 #include <exec/task.h>
 
-#ifndef ARCH_MUTEX
-
-void mutex_acquire(mutex_t* m) {
+__attribute__((weak)) void mutex_acquire(mutex_t* m) {
     while(m->locked) task_yield_noirq(); // mutex is being held - yield to the next task.
     m->locked = 1;
 }
 
-void mutex_release(mutex_t* m) {
+__attribute__((weak)) void mutex_release(mutex_t* m) {
     m->locked = 0;
     task_yield_noirq(); // so we don't starve other tasks
 }
 
-bool mutex_test(const mutex_t* m) {
+__attribute__((weak)) bool mutex_test(const mutex_t* m) {
     return (m->locked);
 }
-
-#endif
